4a6e55a8ab82324c084920a0208e71b0f8f3e034
Federico Nusymowicz <fedenusy@gmail.com>
2013-05-24 01:20:05 -0700

    Restructure app for modularity

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -10,7 +10,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object('flaskr.config')
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
